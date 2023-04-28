7874246e5ed71e6f5836f74f16a4154fe8a3312f
Vinicius-Mance <vinimanceprof@gmail.com>
2023-04-28 17:07:58 -0300

    functional data division by pairs

--- ASCIIReader.py
+++ ASCIIReader.py
@@ -2,15 +2,42 @@
 import pandas as pd
 import re
 
-fileLocation = "./files/isorawdata"
-de = pd.read_csv(fileLocation)
-# print(de)
+def ASCIIReader(data, type):
 
+    # default location file for testing
+    # "./files/isorawdata"
 
-print(str(de.columns[0]))
-# def ASCIIReader():
-#
-#     return "A";
-# chr()
-# Prints the ASCII value of the input character
+    # variable to store the data as a string to be interpreted
+    stringData = ''
+
+    if (type == "rawdata"):
+        de = pd.read_csv(data)
+        stringData = str(de.columns[0])
+    elif (type == "string"):
+        stringData = data
+    else :
+        return "Insert a valid data type"
+
+    arrayData = []
+
+    i = 0
+    while i < len(stringData):
+        pair = ''
+        pair = (pair + str(stringData[i]) + str(stringData[i + 1]))
+        i = i + 2
+        arrayData.append(pair)
+    # loop to separate hexadecimals in pairs
+    #
+    #   for (var i = 0; i < isoString.length; i += 2):
+    #       decimalPairs.push(isoString[i] + isoString[i + 1])
+    #   return decimalPairs;
+    #
+
+    # chr()
+    # Prints the ASCII value of the input character
+
+    return arrayData
+
+print(ASCIIReader("./files/isorawdata","rawdata"))
+
 
