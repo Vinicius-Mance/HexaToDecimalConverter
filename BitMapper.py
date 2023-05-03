877492a9cc715d03a3ea36e2103330393cd30de5
Vinicius-Mance <vinimanceprof@gmail.com>
2023-05-03 19:34:17 -0300

    bit mapper updated to include use of mysql database

--- BitMapper.py
+++ BitMapper.py
@@ -1,44 +1,76 @@
 from HexadecimalToBinary import HexadecimalToBinary
 import pandas as pd
 import re
+import mysql.connector
 
 def BitMapper(ISOString):
 
-    sheetName = "ISO - todos"
-    fileLocation = "files\infotable.xls"
+    # # code to run in case an xls file is necessary to be used
+    # sheetName = "ISO - todos"
+    # fileLocation = "files\infotable.xls"
+    #
+    # # array to access info from fields's data elements
+    # de = pd.read_excel(fileLocation, index_col=0, sheet_name=sheetName)
+    # dataElements = []
+    # for i in de.index:
+    #     dataElements.append(i)
+    #
+    # # array to access info from fields's length
+    # legt = pd.read_excel(fileLocation, index_col=1, sheet_name=sheetName)
+    # length = []
+    # for i in legt.index:
+    #     length.append(i)
+    #
+    # # array to access info from fields's description
+    # desc = pd.read_excel(fileLocation, index_col=2, sheet_name=sheetName)
+    # description = []
+    # for i in desc.index:
+    #     description.append(i)
+    #
+    # # getting binary code by calling hexadecimalToBinary external function
+    # binaryString = HexadecimalToBinary(ISOString)
+    #
+    # # array to be filled and returned when calling BitMapper function
+    # bitMapInfo = []
+    #
+    # for index, i in enumerate(binaryString):
+    #
+    #     # statement to save field if present
+    #     if int(i) == 1:
+    #         item = []
+    #         item.append(dataElements[int(index)])
+    #         item.append(length[int(index)])
+    #         item.append(description[int(index)])
+    #         bitMapInfo.append(item)
+    #
+    # return bitMapInfo
 
-    # array to access info from fields's data elements
-    de = pd.read_excel(fileLocation, index_col=0, sheet_name=sheetName)
-    dataElements = []
-    for i in de.index:
-        dataElements.append(i)
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="iso8583info"
+    )
 
-    # array to access info from fields's length
-    legt = pd.read_excel(fileLocation, index_col=1, sheet_name=sheetName)
-    length = []
-    for i in legt.index:
-        length.append(i)
-
-    # array to access info from fields's description
-    desc = pd.read_excel(fileLocation, index_col=2, sheet_name=sheetName)
-    description = []
-    for i in desc.index:
-        description.append(i)
+    cursor = connection.cursor()
+    query = f'SELECT element_log_iso, length_log_iso, description_log_iso FROM log_iso'
+    cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
 
     # getting binary code by calling hexadecimalToBinary external function
     binaryString = HexadecimalToBinary(ISOString)
 
-    # array to be filled and returned when calling BitMapper function
+    # array to return information
     bitMapInfo = []
 
     for index, i in enumerate(binaryString):
 
         # statement to save field if present
         if int(i) == 1:
-            item = []
-            item.append(dataElements[int(index)])
-            item.append(length[int(index)])
-            item.append(description[int(index)])
-            bitMapInfo.append(item)
+            bitMapInfo.append(result[index])
 
     return bitMapInfo
+
+print(BitMapper("FEFF460128E1E20A0000000000000040"))
